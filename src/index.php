58411c50435a34763c84437e17a77c5c7e8acdb2
Guido van Dijk <funnydutchguy@gmail.com>
2021-02-03 19:44:29 +0100

    added input styling

--- src/index.php
+++ src/index.php
@@ -1,39 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=Open+Sans:wght@400;600;700&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="./assets/styles/main.css">
     <title>Document</title>
 </head>
+
 <body>
     <h1 class="title title--page jazz"><?php echo "home"; ?></h1>
 
     <div class="container">
-
         <div class="row">
-            <div class="col-1"><div style="background-color:red">Test red</div></div>
-            <div class="col-auto"><div style="background-color:blue">Test blue</div></div>
-            <div class="col-1"><div style="background-color:green">Test green</div></div>
+            <div class="col-1">
+                <div style="background-color:red">Test red</div>
+            </div>
+            <div class="col-auto">
+                <div style="background-color:blue">Test blue</div>
+            </div>
+            <div class="col-1">
+                <div style="background-color:green">Test green</div>
+            </div>
         </div>
         <div class="row">
-            <div class="col-6"><div style="background-color:blue">Test blue</div></div>
-            <div class="col-6"><div style="background-color:green">Test green</div></div>
+            <div class="col-6">
+                <div style="background-color:blue">Test blue</div>
+            </div>
+            <div class="col-6">
+                <div style="background-color:green">Test green</div>
+            </div>
         </div>
 
         <button>test</button>
         <button disabled>test</button>
-        
-    <input type="checkbox" name="" id="">
-    <input type="radio" name="" id="">
 
-    <input type="text" name="" placeholder="full name" id="">
+        <input type="text" name="" placeholder="full name" id="">
 
         <button class="button--secondary">test</button>
+        <select name="cars" id="cars">
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="mercedes">Mercedes</option>
+            <option value="audi">Audi</option>
+        </select>
+
+        <label for="1">
+            <input type="checkbox" name="" id="1">
+            <span>test</span>
+        </label>
+
+        <label for="2">
+            <input type="radio" name="" id="2">
+            <span>test</span>
+        </label>
     </div>
+
 
     <script src="./assets/scripts/index.js"></script>
 </body>
+
 </html>
