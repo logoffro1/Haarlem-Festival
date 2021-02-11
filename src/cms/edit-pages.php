cc1249ddae5364d96f34b4acafcfb6603a4763d6
Guido van Dijk <funnydutchguy@gmail.com>
2021-02-11 15:52:17 +0100

    created pages list page

---
+++ src/cms/edit-pages.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=Open+Sans:wght@400;600;700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="/assets/styles/main.css">
+    <title>CMS - Dashboard</title>
+</head>
+
+<body class="page--cms">
+    <aside class="navigation--cms">
+        <header class="navigation--cms__header">
+            <img src="/assets/images/svg/logo.svg" alt="">
+            <a href="#" class="navigation--cms__header__profile">
+                <img src="" alt="">
+            </a>
+        </header>
+        <nav class="navigation--cms__body">
+            <ul>
+                <li><a href="#" class="button button--cms button--active">Edit Pages</a></li>
+                <li><a href="#" class="button button--cms">Events</a></li>
+                <li><a href="#" class="button button--cms">Reservations</a></li>
+                <li><a href="#" class="button button--cms">Invoices</a></li>
+                <li><a href="#" class="button button--cms">Users</a></li>
+                <li><a href="#" class="button button--cms">API</a></li>
+            </ul>
+        </nav>
+        <footer class="navigation--cms__footer">
+            <a href="#" class="button button--secondary">Log out</a>
+        </footer>
+    </aside>
+
+    <section class="cms-container row">
+        <nav class="breadcrumbs breadcrumbs--cms col-12">
+            <ul>
+                <li class="breadcrumbs__breadcrumb"><a href="">Edit Pages</a></li>
+            </ul>
+        </nav>
+
+        <article class="card--cms col-8">
+            <header class="card--cms__header">
+                <h3 class="card--cms__header__title">Pages</h3>
+                <button class="button">Add page</button>
+            </header>
+            <table class="card--cms__body table--cms">
+                <tbody>
+                    <tr>
+                        <td>Homepage</td>
+                        <td class="table--cms__item__navigation">
+                            <a href="cms/artist-performance-details.php" class="">Edit</a>
+                            <a href="#" class="">Remove</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Jazz event page</td>
+                        <td class="table--cms__item__navigation">
+                            <a href="#" class="">Edit</a>
+                            <a href="#" class="">Remove</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </article>
+    </section>
+
+    <div class="notification--cms js-notification">
+        <div class="notification--cms__title">Test title</div>
+        <div class="notification--cms__body">
+            test content <span class="notification--cms__body__important">Bold</span>
+        </div>
+    </div>
+    <script src="/assets/scripts/index.js"></script>
+</body>
+</html>
