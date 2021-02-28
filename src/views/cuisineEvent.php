e80a8e0b10d21a4e82d6453b1795562bbe48554e
Cosmin Ilie <cosmin.ilie2704@gmail.com>
2021-02-28 22:34:50 +0100

    added cuisine event banner

--- src/views/cuisineEvent.php
+++ src/views/cuisineEvent.php
@@ -10,9 +10,12 @@
 ?>
 
 <section class="container section">
+<pre style="letter-spacing:1px"><a href = "#">Events </a> > <a hrtef = "#"> Haarlem Cuisine</a> </pre>
+<h1 class="title title--page cuisine"> The Haarlem Cuisine </h1>
 
-
+<img src="../assets/images/cuisineBanner.png" class="banner" alt="The Haarlem Festival" title="The Haarlem Cuisine">
 </section>
+
 
 <?php 
     $footer = new footer();
