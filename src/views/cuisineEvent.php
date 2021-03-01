00cf0fc21359f892bd7e80a587f667067fa12427
Cosmin Ilie <cosmin.ilie2704@gmail.com>
2021-03-01 11:51:05 +0100

    added design lines on cuisine page

--- src/views/cuisineEvent.php
+++ src/views/cuisineEvent.php
@@ -9,11 +9,17 @@
     $navigation->render();
 ?>
 
-<section class="container section">
+<section class="container section" style="margin-top:-50px">
 <pre style="letter-spacing:1px"><a href = "#">Events </a> > <a hrtef = "#"> Haarlem Cuisine</a> </pre>
 <h1 class="title title--page cuisine"> The Haarlem Cuisine </h1>
-
 <img src="../assets/images/cuisineBanner.png" class="banner" alt="The Haarlem Festival" title="The Haarlem Cuisine">
+
+<hr class="line">
+<h2 style="width:55%;margin:auto;text-align:center">Pay a visit to Haarlem this year and enjoy the art of good food an drinks in one of our many special restaurants, 
+     or be surprised at one of the gastronomic events that the city has to offer.
+</h2>
+<hr class="line">
+
 </section>
 
 
