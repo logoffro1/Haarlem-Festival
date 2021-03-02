3c56f804526d467c03c0bac8fa30ec80a2724752
Cosmin Ilie <cosmin.ilie2704@gmail.com>
2021-03-02 21:36:16 +0100

    finished restaurant card template

--- src/components/checkbox.php
+++ src/components/checkbox.php
@@ -3,19 +3,17 @@
         private string $text;
         private string $name;
         private string $id;
-        private string $class;
 
-        public function __construct(string $text, string $name, string $id, string $class = "") {
+        public function __construct(string $text, string $name, string $id) {
             $this->text = $text;
             $this->name = $name;
             $this->id = $id;
-            $this->class = $class;
         }
         
         public function render()
         {
             echo "
-            <label for='$this->id' class ='$this->class'>
+            <label for='$this->id'>
                 <input type='checkbox' name='$this->name' id='$this->id' >
                 <span >$this->text</span>
             </label>
--- src/views/cuisineEvent.php
+++ src/views/cuisineEvent.php
@@ -12,7 +12,7 @@
 <section class="container section" style="margin-top:-50px">
 <pre style="letter-spacing:1px"><a href = "#">Events </a> > <a hrtef = "#"> Haarlem Cuisine</a> </pre>
 <h1 class="title title--page cuisine"> The Haarlem Cuisine </h1>
-<img src="../assets/images/cuisineBanner.png" class="banner" alt="The Haarlem Festival" title="The Haarlem Cuisine">
+<img src="../assets/images/cuisine/cuisineBanner.png" class="banner" alt="The Haarlem Festival" title="The Haarlem Cuisine">
 
 <hr class="hLine">
 <h2 style="width:55%;margin:auto;text-align:center">Pay a visit to Haarlem this year and enjoy the art of good food an drinks in one of our many special restaurants, 
@@ -22,16 +22,63 @@
 <?php
 //Get cuisine from DB later
 $cuisines = array("All", "Dutch", "French", "Argentinian", "European", "Fish and Seafood", "Steakhouse", "Modern");
-echo "<article style='text-align:center;'>";
+echo "<fieldset class='checkboxes--wrapper' style='text-align:center;'>";
 foreach($cuisines as $cuisine){
-    $checkbox = new checkbox($cuisine,$cuisine,$cuisine,"cuisineCheck");
+    $checkbox = new checkbox($cuisine,$cuisine,$cuisine);
     $checkbox->render();
 }
-echo "</article>";
+echo "</fieldset>";
 ?>
 <hr class="hLine">
 <article class = "vLine"></article>
-<section class = "card--restaurant"></section>
+
+
+<section class = "card--container">
+
+<h1 style="margin-bottom:0px">Restaurant Mr. & Mrs.</h1>
+
+<h3 style="margin-bottom:0px;margin-top:3px;">
+<img src="../assets/images/cuisine/foodIcon.svg" class = "card--foodicon">
+Dutch &#8226 European &#8226 Fish and Seafood</h3>
+<a href = "#">
+<section class = "card--restaurant">
+<img src="../assets/images/cuisine/ratatouille.jpg" class = "card--img">
+
+<article class="right--container">
+    <img src="../assets/images/cuisine/homeIcon.svg" class = "card--homeicon">
+<pre class = "card--address">Lange Veerstraat 4,
+2011 DB Haarlem
+</pre>
+<pre class = "card--sessions"><span style="font-weight: 900;letter-spacing: 3px;">Sessions</span>
+18:00-19:30
+          &#8226
+19:30-21:00
+          &#8226
+21:00-22:30
+</pre>
+<fieldset class = "card--stars">
+<img src="../assets/images/cuisine/starFilled.png" alt="">
+<img src="../assets/images/cuisine/starFilled.png" alt="">
+<img src="../assets/images/cuisine/starFilled.png" alt="">
+<img src="../assets/images/cuisine/starFilled.png" alt="">
+<img src="../assets/images/cuisine/starEmpty.png" alt="">
+</fieldset>
+<fieldset class = "card--duration">
+    1.5 h
+    <img src="../assets/images/cuisine/clockIcon.png" alt="">
+</fieldset>
+<fieldset class = "card--seats">
+    40
+    <img src="../assets/images/svg/icons/people-24px.svg" alt="">
+</fieldset>
+<img src="../assets/images/cuisine/arrowIcon.png" class = "card--arrow">
+<img src="../assets/images/cuisine/orangeSwoosh.png" class = "card--swoosh">
+</article>
+
+
+</section>
+</a>
+</section>
 </section>
 
 
