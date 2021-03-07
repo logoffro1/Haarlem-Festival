578e8ba79148ed413ea0e03a9270b98b037e3b3a
Emre <kutukemre68@gmail.com>
2021-03-07 16:49:48 +0100

    added thumbnails

--- src/service/jazzArtistService.php
+++ src/service/jazzArtistService.php
@@ -8,10 +8,43 @@
             $this->conn = $this->db->getConnection();
         }
     
-        public function getAnArtistById(int $id)
+        public function getAllJazzArtists()
         {
             $songService = new songService();
-            $query = "SELECT * FROM Artists WHERE artist_id='$id'";
+            $performanceService = new jazzPerformanceService();
+
+            $query = "SELECT * FROM Artists WHERE page_id = 3";
+            $result = $this->conn->query($query);
+
+            if($result)
+            {
+                $artists = array();
+                while($row = $result->fetch_assoc())
+                {
+                    $artist = new jazzArtist(
+                    $row["artist_id"],
+                    $row["name"],
+                    $row["biography"],
+                    $row["image"],
+                    $row["thumbnail"],
+                    $row["facebook"],
+                    $row["instagram"],
+                    $row["youtube"],
+                    $songService->getSongsByArtistId((int)$row["artist_id"]),
+                    $performanceService->getAllJazzPerformancesByArtistId((int)$row["artist_id"])
+                    );
+                    $artists[] = $artist;
+                }
+                return $artists;
+            }
+        }
+
+        public function getAJazzArtistById(int $id)
+        {
+            $songService = new songService();
+            $performanceService = new jazzPerformanceService();
+
+            $query = "SELECT * FROM Artists WHERE page_id = 3 AND artist_id='$id'";
             $result = $this->conn->query($query);
 
             if($result)
@@ -28,6 +61,7 @@
                     $row["instagram"],
                     $row["youtube"],
                     $songService->getSongsByArtistId((int)$row["artist_id"]),
+                    $performanceService->getAllJazzPerformancesByArtistId((int)$row["artist_id"])
                     );
                     return $artist;
             }
