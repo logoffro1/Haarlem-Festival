a4cfde19f6083d14ce8b116beb2f6d09154493b8
Guido van Dijk <funnydutchguy@gmail.com>
2021-03-20 15:10:06 +0100

    added purchase queries

--- src/service/purchaseService.php
+++ src/service/purchaseService.php
@@ -19,7 +19,7 @@ include_once '../config/config.php';
         public function getPurchaseList() : array
         {
             // Build query
-            $query = "SELECT id, title FROM pages";
+            $query = "SELECT * FROM purchases";
 
             // Get connection and results
             if ($result = $this->conn->query($query)) {
@@ -29,20 +29,54 @@ include_once '../config/config.php';
                 // fetch results, and loop over it
                 while($row = $result->fetch_assoc()) {
                     // Create puchase classes based on data
-                    $page = array(
-                        (int)$row["id"], 
-                        $row["title"], 
+                    $purchase = new purchase (
+                        (int)$row["purchase_id"], 
+                        $row["name"], 
+                        $row["email"], 
+                        (float)$row["price"], 
+                        (float)$row["discount"], 
+                        $row["is_payed"] == 1, 
                     );
 
-                    // add new page to list
-                    $purchaseList[] = $page;
+                    // add new purchase to list
+                    $purchaseList[] = $purchase;
                 }
 
                 // return array 
                 return $purchaseList;
+            } else {
+                // If connection cannot be established, throw an error
+                throw new Exception('Could not get the purchases. Please try again');
             }
 
             return array();
+        }
+
+        /**
+         * changePurchasePaymentStatus - Updates the payment status of a specific purchase (by id), to true or false
+         * 
+         * @param bool $isPayed - boolean to check if payment is set to True or False.
+         * @param int $purchaseId - id of the selected purchase.
+         */
+        public function changePurchasePaymentStatus(int $isPayed, int $purchaseId)
+        {
+            // Build query
+            $sql = "UPDATE purchases SET is_payed=? WHERE purchase_id=?";
+
+            // Get connection and prepare statement
+            if($query = $this->conn->prepare($sql)) {
+                // Create bind params to prevent sql injection
+                $query->bind_param("ii", 
+                    $isPayed, // In Mysql the boolean is actually converted to a 'Tinyint' that is restricted to values: 0 / 1, so we need to bind it as an Int.
+                    $purchaseId
+                );
+                
+                // Execute query
+                $query->execute();
+            } else {
+                // If connection cannot be established, throw an error
+                throw new Exception('Could not update the payment status. Please try again');
+            }
         }
     }
 ?>
