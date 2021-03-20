faae145b47b73da3c243cb113c7444261b8838d9
Guido van Dijk <funnydutchguy@gmail.com>
2021-03-20 15:28:42 +0100

    added ticket prop, _get and mutate function to purchase class

--- src/model/purchase.php
+++ src/model/purchase.php
@@ -8,13 +8,26 @@
         private bool $isPayed;
         private ?array $tickets;
 
-        public function __construct(int $purchaseId, string $name, string $email, float $price, float $discount, bool $isPayed, ?array $tickets) {
+        public function __get(string $propName)
+        {
+            if(property_exists($this, $propName)) {
+                return $this->$propName;
+            }
+        }
+
+        public function __construct(int $purchaseId, string $name, string $email, float $price, float $discount, bool $isPayed, ?array $tickets = null) {
             $this->purchaseId = $purchaseId;
             $this->name = $name;
             $this->email = $email;
             $this->price = $price;
             $this->discount = $discount;
             $this->isPayed = $isPayed;
+            $this->tickets = $tickets;
+        }
+
+        public function mutateToArray()
+        {
+            return get_object_vars($this);
         }
     }
 ?>
