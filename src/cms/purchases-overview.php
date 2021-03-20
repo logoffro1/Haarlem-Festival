e5a4d88764928b639e4171cda253c6183bf06189
Guido van Dijk <funnydutchguy@gmail.com>
2021-03-20 15:29:17 +0100

    added purchase overview page

--- src/cms/purchases-overview.php
+++ src/cms/purchases-overview.php
@@ -4,44 +4,61 @@ include '../classes/autoloader.php';
 $head = new head("CMS - Dashboard", "page--cms");
 $head->render();
 
-$navigation = new cmsNavigation("Events");
+$navigation = new cmsNavigation("Purchases");
 $navigation->render();
 
+$purchaseController = new purchaseController();
+$purchases = $purchaseController->getPurchaseList();
 
-$restaurantTableList = array();
-$restaurantController = new restaurantController();
-$restaurants = $restaurantController->getRestaurants();
-
-foreach ($danceArtist as $artist) {
-    $danceArtistArray = array();
-    $danceArtistArray[] = $artist->id;
-    $danceArtistArray[] = $artist->name;
-    $danceArtistArray[] = "<div class='table--cms__item__navigation'>
-    <a href='./artist-detail-page.php?id=$artist->id' class=''>Edit</a>
-    </div>";
-    
-    $danceArtistTableList[] = $danceArtistArray;
+if(isset($_POST['submit'])){
+    $purchaseController->changePurchasePaymentStatus();
 }
 
-$restaurantTable = new table('card--cms__body table--cms', ['Name', 'Address', 'Stars', ''], $restaurantTableList);
-
+$breadcrumbsArray = array(
+    array('text' => 'Purchases', 'url' => "#")
+);
+$breadcrumbs = new breadcrumbs($breadcrumbsArray, 'breadcrumbs--cms');
 ?>
 
     <div class="cms-container">
-        <nav class="breadcrumbs breadcrumbs--cms">
-            <ul>
-                <li class="breadcrumbs__breadcrumb"><a href="">Events</a></li>
-            </ul>
-        </nav>
+        <?php
+            $breadcrumbs->render();
+        ?>
        
-        <article class="card--cms col-8">
+        <article class="card--cms col-11">
             <header class="card--cms__header">
-                <h3 class="card--cms__header__title">Tickets</h3>
+                <h3 class="card--cms__header__title">Purchases</h3>
             </header>
            
             <?php
-                $tickets->render();
+                foreach ($purchases as $purchase) {
+                    $purchaseTableList = array();
+                    $purchaseArray = array();
+
+                    $mutatedPurchase = $purchase->mutateToArray();
+                    unset($mutatedPurchase['tickets']);
+
+                    $mutatedPurchase['purchaseId'] = sprintf("<input class='input--small type='text' readonly name='purchaseId' value='%d'>", $purchase->purchaseId);
+
+                    $inputIsChecked = $purchase->isPayed ? "checked" : '';
+                    $mutatedPurchase['isPayed'] = "<input type='checkbox' name='isPayed' $inputIsChecked>";
+
+                    $mutatedPurchase['submit'] = '<input class="button" type="submit" name="submit" value="Update payment status">';
+
+                    $purchaseTableList[] = $mutatedPurchase;
+                    $purchaseTable = new table('card--cms__body table--cms', ['purchase ID', 'name', 'email', 'price', 'discount', 'is payed', ''], $purchaseTableList);
             ?>
+                    <form action="" method="post">
+
+                        <?php
+                            $purchaseTable->render();
+                        ?>
+                        
+                    </form>
+            <?php   
+                }
+            ?>
+
         </article>
         
     </div>
