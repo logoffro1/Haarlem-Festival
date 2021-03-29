360b0a74c6181acd633955e33aab189f88512a64
omaralawin <omaralawin@gmail.com>
2021-03-29 16:48:20 +0300

    improvements to map in indiv artist page

--- src/components/dance/danceExploreMap.php
+++ src/components/dance/danceExploreMap.php
@@ -7,7 +7,7 @@ class danceExploreMap
         echo "
         <section class='container-fluid section exploreHaarlem--dance' style='padding-top:0px;'>
             <h2 style='margin-right:250px;'>Arrive Early, Enjoy More!</h2>
-            <img src='../assets/images/dance/exploreHaarlem/map.jpg'>
+            <img src='../assets/images/dance/exploreHaarlem/Map.png'>
         </section>
         ";
     }
