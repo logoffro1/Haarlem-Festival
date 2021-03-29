c7b02cbd9bbcb826141b2f92aa0319811a9b1bc0
omaralawin <omaralawin@gmail.com>
2021-03-29 15:59:45 +0300

    removed unecessary components

--- src/views/bookmoresavemore.php
+++ src/views/bookmoresavemore.php
@@ -7,8 +7,26 @@
     $navigation = new navigation("Home");
     $navigation->render();
 
-	$bookmoresavemore = new bookmoresavemore();
-	$bookmoresavemore->render();
+    echo "<section class='container section'>
+    <article class='row align-items-left'>
+        <header class='col-6'>
+        <nav class='breadcrumbs'>
+        <ul>
+            <li class='breadcrumbs__breadcrumb'><a href='../views/danceEvent.php'>Artist Overview</a></li>
+            <li class='breadcrumbs__breadcrumb'><a href='#'>Book more, Save more!</a></li>
+        </ul>
+    </nav>
+        <h1 class='title title--page dance'>Book more, Save more!</h1>
+        <p>We have an <b>offer avaliable for all events</b> that can be <b>mix and<br>matched</b> with any other event. For example, you can pair Jazz<br>event tickets with Dance event tickets ands ave on both. These are<br> listed as the '<b>Discount</b>' section on your bill. The more tickets you<br>book, the larger the discount!</p>
+           </header>
+       </article>
+        <article class='row align-items-left'>
+             <header class='col-6'>
+        <h1 class='title title--page dance'>Day tickets</h1>
+        <p><b>Day tickets</b> are <b>cheaper</b> than buying different seperate tickets as it<br>would <b>allow you to access all things related to that specific event<br>on that specific day.</b></p>
+           </header>
+        </article>
+   </section>";
 ?>
 
 <?php
--- src/views/thankyoupage.php
+++ src/views/thankyoupage.php
@@ -10,13 +10,61 @@
 
 	$controller = new thankyoupagecontroller();
 	$controller->sendMail($_SESSION['email'],"Thank you for your purchase!","We look forward to seeing you at our festival ".$_SESSION['fname']." ".$_SESSION['lname'],$_SESSION['fname']);
-	$thankyoupage = new thankyoupage();
-	$thankyoupage->render();
-    //unset($_SESSION['email']);
-    //unset($_SESSION['fname']);
-    //unset($_SESSION['lname']);
-    //unset($_SESSION['dob']);
-    //unset($_SESSION['phoneno']);
+	//$thankyoupage = new thankyoupage();
+	//$thankyoupage->render();
+    echo "		<section class='container-fluid section' style='padding:0px;'>
+		<article class='row align-items-left'>
+			<header class='col-6'>
+					   <p>Payment Information > Thank you</p>
+			<h1 class='title title--page dance'>Payment</h1>
+			   </header>
+			   <img src='../assets/images/dance/paymentprogress2.png' style='width:50%; height:50%;'>
+		   </article>
+		   </section>
+		   <h1 class='title title--page dance' style='margin-left:510px;'>Thank you for your purchase!</h1>
+	   <section class='container section' style='padding:0px; margin-left:auto; margin-right:auto; width:100em; text-align:center;'>
+	   <article class='row'>
+	   <header class='col-12'>
+	   <p style='margin:auto;'> We hope you enjoy the events are the festival!</p>
+	   </header>
+	   </article>
+	   <article class='row'>
+	   <header class='col-12'>
+	   <p> A copy of your receipt has been sent to your e-mail address.</p>
+	   </header>
+	   </article>
+	   <article class='row'>
+	   <header class='col-12'>
+	   <a href='../index.php'>
+	   <button style='margin-top:25px;margin-bottom:25px;'>Return to home</button>
+	   </a>
+	   </header>
+	   </article>
+	   <article class='row'>
+	   <header class='col-12'>
+	   <p> Let your friends know you are attending</p>
+	   </header>
+	   </article>
+	   <article class='row'>
+	   <header class='col-12'>
+	   <p>Share on</p>
+	   </header>
+	   </article>
+	   <article class='row' style='margin-left:730px;'>
+	   <header class='col-1'>
+	   <img src='../assets/images/dance/instagram.png' style='margin-bottom:25px;'>
+	   </header>
+	   <header class='col-1'>
+	   <img src='../assets/images/dance/facebook.png' style='margin-bottom:25px;'>
+	   </header>
+	   </article>
+	   </section>
+	   ";
+    unset($_SESSION['email']);
+    unset($_SESSION['fname']);
+    unset($_SESSION['lname']);
+    unset($_SESSION['dob']);
+    unset($_SESSION['phoneno']);
 ?>
 
 <?php
