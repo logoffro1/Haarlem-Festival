41ecbae2d8b41fc69dcefa9d767127598be267c8
Emre <kutukemre68@gmail.com>
2021-04-02 19:57:45 +0200

    added some stuff back again

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,8 +1,8 @@
-import { notification } from '/notification.js';
-import { breadcrumbs } from '/breadcrumbs.js';
-import { countdown } from '/countdown.js';
-import { dropdown } from '/dropdown.js';
-import { tabs } from '/tabs.js';
+import { notification } from './notification.js';
+import { breadcrumbs } from './breadcrumbs.js';
+import { countdown } from './countdown.js';
+import { dropdown } from './dropdown.js';
+import { tabs } from './tabs.js';
 
 document.addEventListener("DOMContentLoaded", (event) => {
     new notification();
