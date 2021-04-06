6cdcefb9c71b0262e683bfd3e0e96b0fc88a8bce
Guido van Dijk <funnydutchguy@gmail.com>
2021-04-06 19:51:31 +0200

    removed router from index.php

--- src/index.php
+++ src/index.php
@@ -1,20 +1,5 @@
 <?php
 
-require_once "router.php";
-
-route('/', function () {
-    header("Location: /views/homepage.php");
-});
-
-route('/about', function () {
-    return "Hello from the about route";
-});
-
-route('/danceEvent', function () {
-    header("Location: views/danceEvent.php");
-});
-
-$action = $_SERVER['REQUEST_URI'];
-dispatch($action);
+header("Location: /haarlem-festival/views/homepage.php");
 
 ?>
